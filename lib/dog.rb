4301c307fb380b002d523436adf745563f161b1e
Georgette Class-Peters <georgycp@udel.edu>
2020-09-07 18:39:51 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(attributes)
+    #id: nil, name:, breed:
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+      SQL
+
+    DB[:conn].execute(sql)
+  end
